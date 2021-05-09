c052557d3b52a234d2590f4add1ada1247459ae7
VictorMosolea <mvictorandrei@gmail.com>
2021-05-09 12:50:45 +0300

    do extra ex1

--- ex1/featureNormalize.m
+++ ex1/featureNormalize.m
@@ -25,7 +25,14 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
+for i=1:length(mu)
+    mu(i) = mean(X(:,i));
+    sigma(i) = std(X(:,i));
+    X_norm(:,i) = (X_norm(:,i) - mu(i))./sigma(i);
+end
 
+
+    
 
 
 
